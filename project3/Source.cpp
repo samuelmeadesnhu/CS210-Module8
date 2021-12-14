541811fd601563463197df8b0130e2bb599c43d9
samuelmeadesnhu <96152128+samuelmeadesnhu@users.noreply.github.com>
2021-12-14 18:39:31 -0500

    Initial Commit
    Project 3

---
+++ project3/Source.cpp
@@ -0,0 +1,194 @@
+#include <Python.h>
+#include <iostream>
+#include <Windows.h>
+#include <cmath>
+#include <string>
+#include <set>
+
+using namespace std;
+
+/*
+Description:
+	To call this function, simply pass the function name in Python that you wish to call.
+Example:
+	callProcedure("printsomething");
+Output:
+	Python will print on the screen: Hello from python!
+Return:
+	None
+*/
+void CallProcedure(string pName)
+{
+	char* procname = new char[pName.length() + 1];
+	std::strcpy(procname, pName.c_str());
+
+	Py_Initialize();
+	PyObject* my_module = PyImport_ImportModule("PythonCode");
+	PyErr_Print();
+	PyObject* my_function = PyObject_GetAttrString(my_module, procname);
+	PyObject* my_result = PyObject_CallObject(my_function, NULL);
+	Py_Finalize();
+
+	delete[] procname;
+}
+
+/*
+Description:
+	To call this function, pass the name of the Python function you wish to call and the string parameter you want to send
+Example:
+	int x = callIntFunc("PrintMe","Test");
+Output:
+	Python will print on the screen:
+		You sent me: Test
+Return:
+	100 is returned to the C++
+*/
+int callIntFunc(string proc, string param)
+{
+	char* procname = new char[proc.length() + 1];
+	std::strcpy(procname, proc.c_str());
+
+	char* paramval = new char[param.length() + 1];
+	std::strcpy(paramval, param.c_str());
+
+
+	PyObject* pName, * pModule, * pDict, * pFunc, * pValue = nullptr, * presult = nullptr;
+	// Initialize the Python Interpreter
+	Py_Initialize();
+	// Build the name object
+	pName = PyUnicode_FromString((char*)"PythonCode");
+	// Load the module object
+	pModule = PyImport_Import(pName);
+	// pDict is a borrowed reference 
+	pDict = PyModule_GetDict(pModule);
+	// pFunc is also a borrowed reference 
+	pFunc = PyDict_GetItemString(pDict, procname);
+	if (PyCallable_Check(pFunc))
+	{
+		pValue = Py_BuildValue("(z)", paramval);
+		PyErr_Print();
+		presult = PyObject_CallObject(pFunc, pValue);
+		PyErr_Print();
+	}
+	else
+	{
+		PyErr_Print();
+	}
+	//printf("Result is %d\n", _PyLong_AsInt(presult));
+	Py_DECREF(pValue);
+	// Clean up
+	Py_DECREF(pModule);
+	Py_DECREF(pName);
+	// Finish the Python Interpreter
+	Py_Finalize();
+
+	// clean 
+	delete[] procname;
+	delete[] paramval;
+
+
+	return _PyLong_AsInt(presult);
+}
+
+/*
+Description:
+	To call this function, pass the name of the Python functino you wish to call and the string parameter you want to send
+Example:
+	int x = callIntFunc("doublevalue",5);
+Return:
+	25 is returned to the C++
+*/
+int callIntFunc(string proc, int param)
+{
+	char* procname = new char[proc.length() + 1];
+	std::strcpy(procname, proc.c_str());
+
+	PyObject* pName, * pModule, * pDict, * pFunc, * pValue = nullptr, * presult = nullptr;
+	// Initialize the Python Interpreter
+	Py_Initialize();
+	// Build the name object
+	pName = PyUnicode_FromString((char*)"PythonCode");
+	// Load the module object
+	pModule = PyImport_Import(pName);
+	// pDict is a borrowed reference 
+	pDict = PyModule_GetDict(pModule);
+	// pFunc is also a borrowed reference 
+	pFunc = PyDict_GetItemString(pDict, procname);
+	if (PyCallable_Check(pFunc))
+	{
+		pValue = Py_BuildValue("(i)", param);
+		PyErr_Print();
+		presult = PyObject_CallObject(pFunc, pValue);
+		PyErr_Print();
+	}
+	else
+	{
+		PyErr_Print();
+	}
+	//printf("Result is %d\n", _PyLong_AsInt(presult));
+	Py_DECREF(pValue);
+	// Clean up
+	Py_DECREF(pModule);
+	Py_DECREF(pName);
+	// Finish the Python Interpreter
+	Py_Finalize();
+
+	// clean 
+	delete[] procname;
+
+	return _PyLong_AsInt(presult);
+}
+
+
+// handle input
+void inputHandler(int userInput) {
+
+	string key;
+
+
+	//Display menu options
+	switch (userInput) {
+	case 1:
+		// Display list of purchased items
+		CallProcedure("printPurchaseList");
+		break;
+	case 2:
+		// Return key for entered value
+		cout << "Item lookup: " << endl;
+		cin >> key;
+		cout << callIntFunc("printValue", key) << endl;
+		break;
+	//Display text based histogram
+	case 3:
+		CallProcedure("printHistogram");
+		break;
+		// Exit
+	case 4:
+		exit(0);
+		break;
+	}
+}
+
+void main()
+{
+
+	// Initialize menu validation list
+	int userInput = 0;
+	set<int> validInput{ 1, 2, 3, 4 };
+
+	while (validInput.count(userInput) == 0) {
+		CallProcedure("printMenu");
+		cin >> userInput;
+		cin.clear();
+		cin.ignore(10000, '\n');
+		if (userInput == 0) {
+			cout << "Error: please enter a numeric value of 1, 2, 3, or 4." << endl;
+		}
+
+	}
+
+	// Call method to handle user input
+	inputHandler(userInput);
+
+}
+
